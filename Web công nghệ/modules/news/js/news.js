ff4af35b4055c1117f9e3d487c1b495f5a6c6399
Phạm Hoàng Hải sơn <son0949512609@gmail.com>
2017-01-12 20:17:24 +0700

    web cong nghe

---
+++ Web công nghệ/modules/news/js/news.js
@@ -0,0 +1,13 @@
+﻿ 
+function show_comment(id,lang,p) {
+	
+	$.ajax({
+		 type: "GET",
+		 url: ROOT+'modules/news/ajax/_show_comment.php',
+		 data: "id="+id+'&lang='+lang+'&p='+p,
+		 success: function(html){
+			  $("#ext_comment").html(html);
+		 }
+	 });
+}
+
